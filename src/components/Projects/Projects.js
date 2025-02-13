e2831e5fbf16a89212999e66ffbb82c056aa380c
Tisaghu <samasher9@hotmail.com>
2025-02-12 17:58:47 -0800

    Filled in project cards and fixed some styling issues with them.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,19 +6,26 @@ function Projects() {
     // Sample project data
     const projectList = [
         {
-            title: "Project 1",
-            description: "This is a description of project 1",
-            link: "https://www.example.com"
+            title: "Gamified Language Data Explorer",
+            description: `An interactive, gamified platform that enables users to explore the NAU audio-text corpus 
+            for language learning. The tool features personalized accounts, engaging games, and a context-aware search engine to 
+            enhance comprehension and pronunciation. Leveraging AWS, MySQL, and PHP, I optimized backend operations for secure 
+            and efficient content retrieval while ensuring seamless frontend-backend integration for a responsive user experience.`,
+            link: "https://www.ceias.nau.edu/capstone/projects/CS/2023/PhoneticEvolution_S23/"
         },
         {
-            title: "Project 2",
-            description: "This is a description of project 2",
-            link: "https://www.example.com"
+            title: "Space Shooter Game",
+            description: `A top-down space shooter prototype in Unity using C#, incorporating industry-standard game design
+             principles to create engaging and dynamic gameplay. Designed diverse enemy behaviors, power-ups, and balanced mechanics 
+             while leveraging modular development for scalability and future feature expansion.`,
+            link: "https://github.com/Tisaghu/Space-Shooter"
         },
         {
-            title: "Project 3",
-            description: "This is a description of project 3",
-            link: "https://www.example.com"
+            title: "Time Tracker",
+            description: `A web-based time tracking application built with Flask, JavaScript (ES6 modules), and Bootstrap. This app
+             allows users to start, stop, and categorize tracked time, with logs stored in CSV format. Designed with modular architecture,
+              it follows best practices in API design, front-end organization, and asynchronous JavaScript for a smooth user experience.`,
+            link: "https://github.com/Tisaghu/Time-Tracker"
         }
     ];
 
