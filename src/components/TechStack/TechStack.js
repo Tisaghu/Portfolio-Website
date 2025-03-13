d3a45aa415bc62da8e5b91e72e242d220b7f8fbf
Tisaghu <samasher9@hotmail.com>
2025-03-13 11:55:44 -0700

    Added sections for languages and frameworks as well as css. Need to add logo assets.

--- src/components/TechStack/TechStack.js
+++ src/components/TechStack/TechStack.js
@@ -1,29 +1,67 @@
 import React from "react";
 import "./TechStack.css";
 
+//Import logos
+import htmlLogo from "../../assets/html5-logo.png";
+import cssLogo from "../../assets/css3-logo.png";
+import jsLogo from "../../assets/js-logo.png";
+import reactLogo from "../../assets/react-logo.png";
+import pythonLogo from "../../assets/python-logo.png";
+
 function TechStack() {
-    // Sample tech stack data
-    const techList = [
-        "HTML5",
-        "CSS3",
-        "JavaScript (ES6)",
-        "React",
-        "Node.js",
-        "Express",
-        "MongoDB",
-        "MySQL",
-        "Python",
-        "Flask",
-        "Unity",
-        "C#",
-        "AWS",
-        "Bootstrap",
-        "Git"
+    // Categorize technologies
+    const languages = [
+        { name: "HTML", logo: htmlLogo },
+        { name: "CSS", logo: cssLogo },
+        { name: "JavaScript", logo: jsLogo },
+        { name: "Python", logo: pythonLogo },
+        { name: "PHP", logo: "" },
+        { name: "C#", logo: "" },
+        { name: "SQL", logo: "" },
+        { name: "Java", logo: "" },
+    ];
+    const frameworks = [
+        { name: "React", logo: reactLogo },
+        { name: "AWS", logo: "" },
+        { name: "Unity", logo: "" },
+        { name: "Flask", logo: "" },
+        { name: "MySQL", logo: "" },
+        { name: "Bootstrap", logo: "" },
+        { name: "Git", logo: "" }
     ];
 
     return (
         <section className="tech-stack-section">
-        </section>        
+            <h2>My Tech Stack</h2>
+
+            <div className="tech-container">
+                {/* Languages Section */}
+                <div className="tech-box">
+                    <h3>Programming Languages</h3>
+                    <div className="tech-items">
+                        {languages.map((tech, index) => (
+                            <div key={index} className="tech-item">
+                                {tech.logo && <img src={tech.logo} alt={tech.name} />}
+                                <span>{tech.name}</span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                {/* Frameworks & Tools Section */}
+                <div className="tech-box">
+                    <h3>Frameworks & Tools</h3>
+                    <div className="tech-items">
+                        {frameworks.map((tech, index) => (
+                            <div key={index} className="tech-item">
+                                {tech.logo && <img src={tech.logo} alt={tech.name} />}
+                                <span>{tech.name}</span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
