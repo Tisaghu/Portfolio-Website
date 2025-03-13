1a1f16e85cbb64441e512a12c6c481d199e33475
Tisaghu <samasher9@hotmail.com>
2025-03-13 11:40:38 -0700

    Initial setup of component.

---
+++ src/components/TechStack/TechStack.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./TechStack.css";
+
+function TechStack() {
+    // Sample tech stack data
+    const techList = [
+        "HTML5",
+        "CSS3",
+        "JavaScript (ES6)",
+        "React",
+        "Node.js",
+        "Express",
+        "MongoDB",
+        "MySQL",
+        "Python",
+        "Flask",
+        "Unity",
+        "C#",
+        "AWS",
+        "Bootstrap",
+        "Git"
+    ];
+
+    return (
+        <section className="tech-stack-section">
+        </section>        
+    );
+}
+
+export default TechStack;
